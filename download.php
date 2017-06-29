abe3c562be9b472daf1f5249cfe7c52c7ad21237
hyhanyan <2287423675@qq.com>
2017-06-29 16:58:20 +0800

    Update download.php

--- download.php
+++ download.php
@@ -89,15 +89,19 @@ function create_zip($files = array(),$destination = '',$overwrite = false) {
     }    
 }  
 
-function addFileToZip($path,$zip){
+function addFileToZip($path,$zip,$localpath){
     $handler=opendir($path); //打开当前文件夹由$path指定。
     while(($filename=readdir($handler))!==false){
         if($filename != "." && $filename != ".."){//文件夹文件名字为'.'和‘..’，不要对他们进行操作
             if(is_dir($path."/".$filename)){// 如果读取的某个对象是文件夹，则递归
-                addFileToZip($path."/".$filename, $zip);
+                $file_info_arr= pathinfo($path."/".$filename); 
+                $currentpath =$file_info_arr['basename'];
+                $zip->addEmptyDir($localpath."/".$currentpath); 
+                addFileToZip($path."/".$filename, $zip,$localpath."/".$currentpath);
             }else{ //将文件加入zip对象
-                $zip->addFile($path."/".$filename);
-            }
+                $zip->addFile($path."/".$filename,$localpath."/".$filename);  //addFile 的第二个参数(默认使用原始路径名)，ZIP归档中的本地名，它将覆盖文件名
+				////$zip->addFile($file,$new_file);   $new_file 将会 覆盖 $file 的文件名，所以我们这里加入相应的路径名即可，即 "路径名" + "/" + "文件名"
+             }
         }
     }
     @closedir($path);
@@ -106,31 +110,33 @@ function addFileToZip($path,$zip){
 function create_zip1($files = array(),$destination = '',$overwrite = false) {        
     //如果zip文件已经存在并且设置为不重写返回false    
     if(file_exists($destination) && !$overwrite) { return false; }    
-    //如果存在真实有效的文件    
-    if(count($files)) {      
-        $zip = new ZipArchive();    
-        //打开文件       如果文件已经存在则覆盖，如果没有则创建    
-        if($zip->open($destination,$overwrite ? ZIPARCHIVE::OVERWRITE : ZIPARCHIVE::CREATE) !== true) {    
+        //如果存在真实有效的文件    
+        if(count($files)) {      
+            $zip = new ZipArchive();    
+            //打开文件       如果文件已经存在则覆盖，如果没有则创建    
+            if($zip->open($destination,$overwrite ? ZIPARCHIVE::OVERWRITE : ZIPARCHIVE::CREATE) !== true) {    
+                return false;    
+            }    
+            //向压缩文件中添加文件    
+            foreach($files as $file) { 
+                $file_info_arr= pathinfo($file); 
+                $localpath =$file_info_arr['basename'];
+                if(is_dir($file)){    
+                    $zip->addEmptyDir($localpath); 
+                    addFileToZip($file,$zip,$localpath);
+                }else{
+                    $zip->addFile($file,$localpath);
+                }
+            }    
+            //关闭文件    
+            $zip->close();    
+            //检测文件是否存在    
+            return file_exists($destination);    
+        }else{    
+            //如果没有真实有效的文件返回false    
             return false;    
         }    
-        //向压缩文件中添加文件    
-        foreach($files as $file) { 
-            if(is_dir($file)){
-                addFileToZip($file,$zip);
-            }else{
-                $zip->addFile($file);
-            }
-        }    
-        //关闭文件    
-        $zip->close();    
-        //检测文件是否存在    
-        return file_exists($destination);    
-    }else{    
-        //如果没有真实有效的文件返回false    
-        return false;    
-    }    
 }
-
 
 $file = array();
 
@@ -147,8 +153,8 @@ echo create_zip($file,$zip);
 
 exec("mv $zip hy_download/");
 
-file_dir[]="han"; //文件夹和文件名 都行
-file_dir[]="hypot"; // 文件夹和文件名 都行
+file_dir[]="han"; //文件夹和文件名 都行 绝对路径也可以
+file_dir[]="hypot"; // 文件夹和文件名 都行 绝对路径也可以
 $zip_name = time() . "hh.zip";
 
 echo create_zip1($file,$zip_name); 
